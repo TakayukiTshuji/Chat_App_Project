8f3184081d441ca13e332feaedf2999d499015d0
Fujimon <fujimoto41@outlook.jp>
2023-05-02 11:21:26 +0900

    Add :

---
+++ src/Backend/ChatAppLib/ChatAppExtensions.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChatAppLib
+{
+    public static class ChatAppExtensions
+    {
+        public static void ReadLine(string title){
+            Console.WriteLine("Hello!");
+        }
+    }
+}
