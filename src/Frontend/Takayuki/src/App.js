05a9ff4458e1c75876220b950bf8cbaa2157b3f3
TakayukiTshuji <gp22a109@oecu.jp>
2024-02-13 00:04:28 +0900

    change: cookieとページ遷移を行うように変更

--- src/Frontend/Takayuki/src/App.js
+++ src/Frontend/Takayuki/src/App.js
@@ -3,12 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import GroupButton from "./GroupButton";
 import LoginForm from "./LoginForm";
 import MessageApp from "./MessageApp";
+import CreateUser from "./CreateUser";
+import Room from "./Room";
+import Addgrp from "./Addgrp";
 
 function App() {  
   return (
     <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<LoginForm/>}/>
+        <Route path={'/Room'} element={<Room/>}/>
+        <Route path={'/AddGroup'} element={<Addgrp/>}/>
+        <Route path={'/session'} element={<CreateUser/>}/>
         <Route path={'/chat'} element={<MessageApp/>}/>
       </Routes>
      </BrowserRouter>
@@ -16,4 +22,4 @@ function App() {
   //<GroupButton/>
 }
 
-export default App;
+export default App
--- src/Frontend/Takayuki/src/MessageApp.js
+++ src/Frontend/Takayuki/src/MessageApp.js
@@ -1,21 +1,18 @@
 import React, { useState,useEffect,useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 import "./Message.css";
-
-function setCookie(name, value, days) {
-  const expirationDate = new Date();
-  expirationDate.setDate(expirationDate.getDate() + days);
-  const cookie = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`;
-  document.cookie = cookie;
-}
 
 //--------
 /*GETを行う*/
 //--------
-const MessageApp = () => {
+const MessageApp = ({room}) => {
   const [usermessages, setUserMessages] = useState([]);
   const [inputValue, setInputValue]     = useState("");
   const [sender, setSender]             = useState([]);
   const messagesEndRef = useRef(null);
+  const navigate = useNavigate();
+  const [cookies,setCookie,removeCookie]=useCookies();
 
   const scrollToBottom = () => {
     if (messagesEndRef.current) {  // messagesEndRef.current が null でないことを確認
@@ -24,21 +21,17 @@ const MessageApp = () => {
   };
 
   const getMessages= ()=>{
-    fetch('https://localhost:7038/api/ChatCtl?room=0',{
-      credentials:'include'
+    fetch(`https://localhost:7038/api/ChatCtl?room=${room}`,{
+      method:'GET',
+      credentials: 'include',
     })
     .then((response)=>{
       return response.json();
     })
     .then((data)=>{
-      const stermessa=data.message;
-      setUserMessages([...usermessages,...stermessa]);
-      if (data.status && Array.isArray(data.result)) {
-        console.log("stermessa:",data); // resultの中身をログに表示
-      } else {
-        console.error("Invalid data format:", data);
-      }
-      console.log("Response data:", data);
+      console.log("Response data:", data);//ここでクッキーが存在しないと表示される
+      const stermessa=data.result.map(e=>e.message);//resultの中のメッセージを取り出し
+      setUserMessages([...stermessa]);//メッセージを表示するために上の変数をここに入れる。
     })
     .catch((error)=>{console.log("Error feth",error)})
     .finally(() => scrollToBottom());
@@ -49,66 +42,73 @@ const MessageApp = () => {
   //レンダリングを行う
   //
   useEffect(() => {
-    /*const intervalId = setInterval(() => {
-      getMessages();
-    }, 10000);*/
+
+    //テスト用
+    fetch('https://localhost:7038/api/ChatSessionCtl',{
+      mode:'cors',
+      credentials:'include',
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      console.log('sck',data);
+      //setCookie('session_id',data.result.sessionId);
+    })
+    //const intervalId = setInterval(() => {
+    //  getMessages();
+    //}, 10000);
   
     // 初回実行時にもgetMessagesを呼び出す
-    getMessages();
+    //getMessages();
   
     // コンポーネントがアンマウントされたときにクリーンアップ
     //return () => clearInterval(intervalId);
   },[]);
 
-  //
-  //cookieをいれる
-  //
-  const setSessionCookie = (sessionId) => {
-    setCookie("session_id", sessionId, 30); // 有効期限は7日間
-  };
-
   //----------
   /*POSTを行う*/
   //----------
   const handleMessageSend = () => {
-    const newMessage = {
-      "message":inputValue,
-      "room":0
-    };
-
-    fetch(`https://localhost:7038/api/ChatCtl?message=${inputValue}&room=0`, {
+    fetch(`https://localhost:7038/api/ChatCtl?message=${inputValue}&room=${room}`, {
       method: 'POST',
       credentials:'include',//sameoriginはクロスリジンには送信しない。
       headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
         //"Cookie": "session_id=<65cc897e-6225-4e14-b788-fdf7d2da24a2>",
-        // 'Access-Control-Allow-Origin': 'http://localhost:7038',
+        //'Access-Control-Allow-Origin': 'http://localhost:7038',
         // 'Access-Control-Request-Method': 'POST',
-        // 'Access-Control-Allow-Credentials':'true',
-      },
-      body: JSON.stringify(newMessage),
-      
+        //'Access-Control-Allow-Credentials':'true',
+      },      
     })
-    //TODO存在しないセッションIDと言われてる
     .then((response) => {
       // サーバーからのレスポンスヘッダーに含まれるCookieを取得
-      const sessionCookie = response.headers.get('session_id');
-      if (sessionCookie) {
+      //const sessionCookie = response.headers.get('session_id');
+      //if (sessionCookie) {
         // クッキーが存在する場合、セッションクッキーを設定
-        setSessionCookie(sessionCookie);
-      }
+      //  setSessionCookie(sessionCookie);
+      //}
       return response.json();
     })
     .then((data) => {
-      console.log("data",data);
-      const userName=data.result.user;
+      console.log("data:",data);
+      //const userName=data.result.user;
       setInputValue("");
-      setSender(...sender,userName);
+      //setSender(...sender,userName);
     })
     .catch((error) => {console.error("Error sending message:", error)})
   };
 
+  const Logout=()=>{
+    removeCookie('sessionId');
+    navigate('/');
+  }
+
+  
   //
   //表示させる
   //
@@ -121,6 +121,10 @@ const MessageApp = () => {
              </div>
         ))}
         <div ref={messagesEndRef} />
+      </div>
+      <div>
+        <button onClick={getMessages}>更新</button>
+        <button onClick={Logout}>ログアウト</button>
       </div>
       
       <div className="input-container">
