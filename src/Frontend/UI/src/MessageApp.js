bf4ee150c23707a3a785c73f7de15ed7f25fdcd7
TakayukiTshuji <gp22a109@oecu.jp>
2024-06-19 16:17:17 +0900

    Add：各機能の追加（削除されたので一旦追加させました）

---
+++ src/Frontend/UI/src/MessageApp.js
@@ -0,0 +1,127 @@
+//Reactフレームワーク
+import React, { useState,useEffect,useRef } from "react";
+//パッケージ
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useLocation } from "react-router-dom";
+//CSS
+import "./Message.css";
+
+
+const MessageApp = () => {
+  const [usermessages, setUserMessages] = useState([]);
+  const [inputValue, setInputValue]     = useState("");
+  const messagesEndRef = useRef(null);
+  const navigate = useNavigate();
+  const [cookies,setCookie,removeCookie]=useCookies();
+  const location = useLocation();
+  const roomName = location.state.name;
+
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {  // messagesEndRef.current が null でないことを確認
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+    }
+  };
+  
+  //--------
+  /*GETを行う*/
+  //--------
+  const getMessages= ()=>{
+    fetch(`https://localhost:7038/api/ChatCtl?room=${roomName}`,{
+      method:'GET',
+      credentials: 'include',
+    })
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      console.log("Response data:", data);
+      const stermessa=data.result.map(e=>({
+        message:e.message,
+        user:e.user
+      }));//resultの中のメッセージを取り出し
+      setUserMessages([...stermessa]);//メッセージを表示するために上の変数をここに入れる。
+    })
+    .catch((error)=>{console.log("Error feth",error)})
+    .finally(() => scrollToBottom());
+  };
+
+  
+  //
+  //レンダリングを行う
+  //
+  useEffect(() => {
+    const timer = setInterval(()=>{
+      getMessages();
+    },3000);
+    return ()=>{
+      clearInterval(timer);
+    }
+  },[]);
+
+  //----------
+  /*POSTを行う*/
+  //----------
+  const handleMessageSend = () => {
+    fetch(`https://localhost:7038/api/ChatCtl?message=${inputValue}&room=${roomName}`, {
+      method: 'POST',
+      credentials:'include',//sameoriginはクロスリジンには送信しない。
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },      
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      //console.log("data:",data);
+      //const userName=data.result.user;
+      setInputValue("");
+      //setSender(...sender,userName);
+    })
+    .catch((error) => {console.error("Error sending message:", error)})
+  };
+
+  const Logout=()=>{
+    removeCookie('sessionId');
+    navigate('/');
+  }
+
+  
+  //
+  //表示させる
+  //
+  return (
+    <div>
+      <div className="message-container">
+        {usermessages.map((data,index)=>(
+              <div key={index} className="message">
+                <strong>{data.user}</strong> <p>{data.message}</p>
+             </div>
+        ))}
+        <div ref={messagesEndRef} />
+      </div>
+      <div>
+        <button onClick={getMessages}>更新</button>
+        <button onClick={Logout}>ログアウト</button>
+      </div>
+      
+      <div className="input-container">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="入力"
+          className="input-text"
+        />
+        <button onClick={handleMessageSend} className="send-button">
+          Send
+        </button>        
+      </div>
+    </div>
+  );
+};
+
+export default MessageApp;
---
+++ src/Frontend/UI/src/Room.js
@@ -0,0 +1,109 @@
+//Reactフレームワーク
+import React, {useState,useRef, useEffect}from 'react';
+//パッケージ
+import { useCookies } from 'react-cookie';
+import { json, useNavigate } from 'react-router-dom';
+//外部フレームワーク
+import { motion,AnimatePresence } from 'framer-motion';
+
+const Room = () => {
+    const messagesEndRef = useRef(null);
+    const [room,setroom]=useState([]);
+    const [unionRoom,setunionRoom]=useState([]);
+    const [cookies,setCookie,removeCookie]=useCookies(['session_id']);
+    const navigate = useNavigate();
+    let roomNameID=[];
+
+
+    const scrollToBottom = () => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+        }
+    };
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            getRoom();
+        },5000);
+    },[])
+
+    const getRoom=()=>{
+        fetch(`https://localhost:7038/api/ChatRoomCtl`,{
+            credentials:'include',
+            headers:{
+                'Content-Type':'application/json',
+            }
+        })
+        .then((response)=>{
+            return response.json();
+        })
+        .then((data)=>{
+            console.log("successRoomData:",data);
+            const roomName=data.result.map(e=>e.name);
+            roomNameID=data.result.map(e=>e.room_id);
+            setroom([...roomName]);
+            setunionRoom([...roomName]);
+        })
+        .catch((error)=>{console.log("error:",error)})
+        .finally(()=>scrollToBottom());
+    }
+
+    const handleRoomClick=(roomid)=>{
+        const Romnum=unionRoom.indexOf(roomid);
+        if((Romnum!==-1)){
+            console.log("成功")
+        }else{
+            console.log("ありませんでした。");
+        }
+        
+        //roomidは名前だけ取っているので、これがどこの番号か知りたい
+        console.log(roomid);
+        navigate('/chat',{state:{name:Romnum}});
+    }
+    const changeAddRom=()=>{
+        navigate('/AddGroup');
+    }
+    const Logout=()=>{
+        removeCookie('sessionId');
+        navigate('/');
+    }
+    const DeleteRoom=()=>{
+
+        fetch("https://localhost:7038/api/ChatRoomCtl/Delete",{
+            method:'DELETE',
+            credentials:'include',
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        .then((response)=>{
+            console.log("削除",response);
+            return json(response);
+        })
+
+
+    }
+    
+    return (
+        <div>
+            <h1>Room</h1>
+            <button onClick={Logout}>ログアウト</button>
+            <button onClick={changeAddRom}>追加</button>
+            <button onClick={getRoom}>更新</button>
+            <div>
+            <motion.section
+                initial={{ opacity: 0 }} // 初期状態
+                animate={{ opacity: 1 }} // マウント時
+                exit={{ opacity: 0 }}    // アンマウント時
+            >
+                {room.map((data, index) => (
+                <button key={index} onClick={() => handleRoomClick(data)}>{data}</button>
+                ))}
+            </motion.section>
+            
+            </div>
+        </div>
+    )
+}
+
+export default Room
